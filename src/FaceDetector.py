0d65ce12e585df4913724c79254364e5d192a4ac
freearhey <7253922+freearhey@users.noreply.github.com>
2023-08-02 05:42:13 +0300

    Update FaceDetector.py

--- src/FaceDetector.py
+++ src/FaceDetector.py
@@ -1,33 +1,50 @@
-from .util import ( detector, align, geometry )
+from retinaface import RetinaFace
+from .utils import (calc_distance, calc_angle, calc_bbox, calc_coords)
 
 def detect(img):
-  output = []
-  faces = detector.detect_faces(img)
-  for face in faces:
-    score, orig_bbox, landmarks = face
+  results = RetinaFace.detect_faces(img, threshold = 0.95, allow_upscaling=False)
+
+  faces = []
+  if type(results) != dict:
+    return faces
+
+  for face in results.values():
+    x1, y1, x2, y2 = face['facial_area']
+    landmarks = {}
+    for key in face['landmarks']:
+      landmarks[key] = tuple(face['landmarks'][key])
+
+    landmarks = add_extra_landmarks(landmarks)
+
     pivot = landmarks['eyes_center']
-    angle = align.calc_angle(landmarks)
+    angle = calc_angle(landmarks)
+    coords = calc_coords(landmarks, pivot, angle)
+    bbox = calc_bbox(coords)
+    pivot = landmarks["face_center"]
 
-    eye_line = [landmarks['left_eye'], landmarks['right_eye']]
-    face_line = [landmarks['eyes_center'], landmarks['mouth_center']]
-    face_line_length = geometry.distance(face_line)
-    eye_line_length = geometry.distance(eye_line)
-    max_length = max(face_line_length, eye_line_length)
-
-    bbox_size = int(max_length / .30)
-    bbox_offset_x = int(bbox_size / 2)
-    bbox_offset_y = int(bbox_size * .55)
-    bbox = [(0-bbox_offset_x,0-bbox_offset_y),(bbox_size-bbox_offset_x,0-bbox_offset_y),(bbox_size-bbox_offset_x,bbox_size-bbox_offset_y),(0-bbox_offset_x,bbox_size-bbox_offset_y)]
-    bbox = geometry.move(bbox, (pivot[0], pivot[1]+face_line_length/2))
-    bbox = geometry.rotate(bbox, pivot, angle)
-
-    output.append({
-      "score": score,
+    faces.append({
+      "score": face['score'],
       "angle": angle,
-      "size": bbox_size,
       "pivot": landmarks["face_center"],
+      "coordinates": coords,
       "bounding_box": bbox,
       "landmarks": landmarks
     })
 
-  return output
+  return faces
+
+def add_extra_landmarks(landmarks):
+  #left eye is the eye appearing on the left (right eye of the person)
+  left_eye_x, left_eye_y = landmarks["right_eye"]
+  right_eye_x, right_eye_y = landmarks["left_eye"]
+  left_mouth_x, left_mouth_y = landmarks["mouth_right"]
+  right_mouth_x, right_mouth_y = landmarks["mouth_left"]
+  nose_x, nose_y = landmarks["nose"]
+
+  eyes_center_x, eyes_center_y = (int((left_eye_x + right_eye_x) / 2), int((left_eye_y + right_eye_y) / 2))
+  mouth_center_x, mouth_center_y = (int((left_mouth_x + right_mouth_x) / 2), int((left_mouth_y + right_mouth_y) / 2))
+  landmarks['eyes_center'] = (eyes_center_x, eyes_center_y)
+  landmarks['mouth_center'] = (mouth_center_x, mouth_center_y)
+  landmarks['face_center'] = (int((eyes_center_x + mouth_center_x) / 2), int((eyes_center_y + mouth_center_y) / 2))
+
+  return landmarks
